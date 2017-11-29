c2f29710c5ff94c4228d109fce4524dc489db49a
Colin Thornton <colinthorntontx@gmail.com>
2017-11-29 15:20:17 +0900

    Add populate route and redirect functionality

--- app.js
+++ app.js
@@ -18,36 +18,69 @@ const testDB = [
 let nextRoute = 4;
 
 app.get("/", (req, res) => {
-  res.redirect(testDB[0].original);
+  res.send('feed me');
 });
 
-app.get("/https://:site", (req, res) => {
-  res.send(req.params.site);
-});
-
-app.get("/:query", (req, res) => {
-  console.log(req.params.query);
-  if (req.params.query.match(/https?:\/\/\w+\.\w+\.*\w*/)) {
+app.get("/https://:url", (req, res) => {
+  console.log(req.params.url);
+  if (req.params.url.match(/\w+\.\w+\.*\w*/)) {
     testDB.push({
-      original: req.params.query,
+      original: `https://${req.params.url}`,
       route: nextRoute,
     });
     nextRoute = nextRoute + 1;
     res.json({
-      original: req.params.query,
-      short: `localhost:8080/${nextRoute - 1}`,
+      original: `https://${req.params.url}`,
+      short: `localhost:8080/${(nextRoute - 1).toString(36)}`,
     });
   } else {
-    let found = testDB.filter((obj) => {
-      return obj.route === Number(req.params.query);
+    res.json({
+      error: true,
+    })
+  }
+});
+
+app.get("/http://:url", (req, res) => {
+  console.log(req.params.url);
+  if (req.params.url.match(/\w+\.\w+\.*\w*/)) {
+    testDB.push({
+      original: `http://${req.params.url}`,
+      route: nextRoute,
     });
-    if (found.length === 1) {
-      res.redirect(found[0].original);
-    } else {
-      res.json({
-        error: true,
-      });
-    }
+    nextRoute = nextRoute + 1;
+    res.json({
+      original: `http://${req.params.url}`,
+      short: `localhost:8080/${(nextRoute - 1).toString(36)}`,
+    });
+  } else {
+    res.json({
+      error: true,
+    })
+  }
+});
+
+app.get("/populate", (req, res) => {
+  for (let i = 0; i < 100; i++) {
+    testDB.push({
+      original: "https://www.google.com",
+      route: nextRoute,
+    });
+    nextRoute++;
+  }
+  res.send(`testDB is ${testDB.length} entries long.`);
+});
+
+app.get("/:query", (req, res) => {
+  console.log(req.params.query);
+  const found = testDB.filter((obj) => {
+    return obj.route === parseInt(req.params.query, 36);
+  });
+  if (found.length === 1) {
+    res.redirect(found[0].original);
+  } else {
+    res.json({
+      error: true,
+    });
   }
 });
 
